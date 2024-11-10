2047dc9b1430cff4166b97c7b41bb552e75aa2cd
Umesh Singh Verma <119877487+umeshSinghVerma@users.noreply.github.com>
2024-11-10 10:15:02 +0530

    extension repository initialization

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import Intract from './components/Intract';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <HashRouter
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true
+      }}
+    >
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/guide" element={<div>Hi, I am on the guide page</div>} />
+        <Route path="/intract" element={<Intract/>}/>
+      </Routes>
+    </HashRouter>
+  </React.StrictMode>
+);
+
+
